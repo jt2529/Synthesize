8da070b9fbe5b9d0e606750de4ef1c0c69ff0417
jt2529 <jj_t_2529@yahoo.com>
2017-02-16 17:18:03 -0600

    Initial Push

---
+++ Assets/Scripts/PlayerAbilities.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAbilities : MonoBehaviour {
+
+    AudioSource[] notes;
+
+    // Use this for initialization
+    void Start () {
+        notes = GameObject.FindGameObjectWithTag("Keytar").GetComponents<AudioSource>();
+        
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // For the F scale (in this test) the notes will be: 7,9,10,12,14,16,17
+        if (Input.GetKeyDown(KeyCode.U)) {
+            notes[5].Play();
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            notes[7].Play();
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            notes[9].Play();
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            notes[10].Play();
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            notes[12].Play();
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            notes[14].Play();
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            notes[16].Play();
+        }
+        if (Input.GetKeyDown(KeyCode.Semicolon))
+        {
+            notes[17].Play();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerPhysics.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(PlayerPhysicsController))]
+public class PlayerPhysics : MonoBehaviour {
+
+    public float maxJumpHeight = 4;
+    public float minJumpHeight = 1;
+    public float timeToJumpApex = .4f;
+    public float accelerationTimeAirborne = .1f;
+    public float accelerationTimeGrounded = .05f;
+    public float moveSpeed = 6;
+
+    float gravity;
+    float maxJumpVelocity;
+    float minJumpVelocity;
+
+    Vector3 velocity;
+
+    float velocityXSmoothing;
+
+    PlayerPhysicsController controller;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<PlayerPhysicsController>();
+
+        gravity = -(2 * maxJumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        maxJumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+        minJumpVelocity = Mathf.Sqrt(2 * Mathf.Abs(gravity) * minJumpHeight);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (controller.collisions.above || controller.collisions.below) {
+            velocity.y = 0;
+        }
+
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetKeyDown(KeyCode.Space) && controller.collisions.below) {
+            velocity.y = maxJumpVelocity;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            if (velocity.y > minJumpVelocity) {
+                velocity.y = minJumpVelocity;
+            }
+        }
+
+        float targetVelocityX = input.x * moveSpeed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)? accelerationTimeGrounded : accelerationTimeAirborne);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+	}
+}
