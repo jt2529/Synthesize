3c20b46e42c550a7669ae76de9975deb8038c9a6
jt2529 <jj_t_2529@yahoo.com>
2017-02-17 14:53:14 -0600

    Music Script with Key Info, Test Queue for Key Mechanics

---
+++ Assets/Scripts/Level1Music.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level1Music : MonoBehaviour {
+
+    public enum Key { major, minor };
+    Key songKey;
+    int startingNote;
+
+	// Use this for initialization
+	void Start () {
+        songKey = Key.major;
+        startingNote = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public Key getSongKey() {
+        return songKey;
+    }
+
+    public int getStartingNote() {
+        return startingNote;
+    }
+}
---
+++ Assets/Scripts/PlayerAbilitiesController.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyQueue {
+    Queue lastKeys;
+    int currentSize;
+
+    public KeyQueue() {
+        lastKeys = new Queue(3);
+        currentSize = 0;
+    }
+
+    public void newNotePlayed(int newNote) {
+        if (currentSize > 2)
+        {
+            lastKeys.Dequeue();
+            currentSize--;
+        }      
+        lastKeys.Enqueue(newNote);
+        currentSize++;
+        Debug.Log(currentSize);
+    }
+
+    public void clearQueue() {
+        lastKeys.Clear();
+    }
+
+    public int[] getNotes() {
+
+        int[] notesArray = new int[3];
+        for (int i = 0; i < currentSize; i++) {
+            notesArray[i] = (int)lastKeys.Dequeue();
+        }
+
+        currentSize = 0;
+        return notesArray;
+    }
+}
+
+public class PlayerAbilitiesController : MonoBehaviour {
+
+    KeyQueue keyQueue;
+    AudioSource[] notes;
+    Level1Music music;
+    Level1Music.Key songKey;
+    int startingNote;
+
+    // Use this for initialization
+    void Start () {
+        keyQueue = new KeyQueue();
+        notes = GameObject.FindGameObjectWithTag("Keytar").GetComponents<AudioSource>();
+        music = GameObject.FindGameObjectWithTag("Music").GetComponent<Level1Music>();
+        songKey = music.getSongKey();
+        startingNote = music.getStartingNote();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            notes[startingNote].Play();
+            keyQueue.newNotePlayed(startingNote);
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            notes[startingNote+2].Play();
+            keyQueue.newNotePlayed(startingNote+2);
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            //For Major keys play 5th note
+            if (songKey == Level1Music.Key.major)
+            {
+                notes[startingNote+4].Play();
+                keyQueue.newNotePlayed(startingNote + 4);
+            }
+            //For Minor keys play 4th note
+            if (songKey == Level1Music.Key.minor)
+            {
+                notes[startingNote+3].Play();
+                keyQueue.newNotePlayed(startingNote + 3);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            notes[startingNote+5].Play();
+            keyQueue.newNotePlayed(startingNote + 5);
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            notes[startingNote+7].Play();
+            keyQueue.newNotePlayed(startingNote + 7);
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            //For Major keys play 8thth note
+            if (songKey == Level1Music.Key.major)
+            {
+                notes[startingNote + 9].Play();
+                keyQueue.newNotePlayed(startingNote + 9);
+            }
+            //For Minor keys play 9th note
+            if (songKey == Level1Music.Key.minor)
+            {
+                notes[startingNote + 8].Play();
+                keyQueue.newNotePlayed(startingNote + 8);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            //For Major keys play 10th note
+            if (songKey == Level1Music.Key.major)
+            {
+                notes[startingNote + 11].Play();
+                keyQueue.newNotePlayed(startingNote + 11);
+            }
+            //For Minor keys play 11th note
+            if (songKey == Level1Music.Key.minor)
+            {
+                notes[startingNote + 10].Play();
+                keyQueue.newNotePlayed(startingNote + 10);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Semicolon))
+        {
+            notes[startingNote + 12].Play();
+            keyQueue.newNotePlayed(startingNote + 12);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            int[] notes = keyQueue.getNotes();
+        }
+    }  
+    
+}
