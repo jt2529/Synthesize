ea12260844bb79997db229dde943f7641de9777f
DadMuscles <zcutshall@gmail.com>
2018-10-04 14:22:24 -0500

    Added a toggled platform. Pressing V extends the platform and activates the collider, or retracts and deactivates.

---
+++ Assets/DeployPlatform.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeployPlatform : MonoBehaviour {
+
+    private Animator animator;
+    public bool deployed = false;
+    private EdgeCollider2D edge;
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        edge = GetComponent<EdgeCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            if (!deployed)
+            {
+                deployed = true;
+                edge.enabled = true;
+            }
+            else
+            {
+                deployed = false;
+                edge.enabled = false;
+            }
+
+            animator.SetBool("deployed", deployed);
+
+        }
+	}
+}
