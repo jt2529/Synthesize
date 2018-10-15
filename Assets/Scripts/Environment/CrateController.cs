1326e362a36cca6dd6bd94ae722c79fb5cef784d
DadMuscles <zcutshall@gmail.com>
2018-10-15 13:42:45 -0500

    Removed duplicate toggler script

---
+++ Assets/Scripts/Environment/CrateController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrateController : MonoBehaviour {
+
+    private Rigidbody2D rBody;
+
+	// Use this for initialization
+	void Start () {
+        rBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Interface/TextTyper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TextTyper : MonoBehaviour {
+
+    public float letterPause = 0.2f;
+    public AudioClip typeSound1;
+    public AudioClip typeSound2;
+
+    string message;
+    TextMesh textComp;
+
+    // Use this for initialization
+    void Start()
+    {
+        textComp = GetComponent<TextMesh>();
+        message = textComp.text;
+        textComp.text = "";
+        StartCoroutine(TypeText());
+    }
+
+    IEnumerator TypeText()
+    {
+        foreach (char letter in message.ToCharArray())
+        {
+            textComp.text += letter;
+            yield return 0;
+            yield return new WaitForSeconds(letterPause);
+        }
+    }
+
+}
