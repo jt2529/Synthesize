8152461fbee2247a02cdce5d06215b2461d8bb04
DadMuscles <zcutshall@gmail.com>
2019-03-02 18:14:58 -0600

    Dumping all changes because I need to reformat thic PC. I'll fix it in a bit.

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+
+    public PlatformNodeManager nodeManager;
+
+    private MovementPhysics controller;
+
+    private GameObject leftTargetNode;
+    private GameObject rightTargetNode;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<MovementPhysics>();
+        leftTargetNode = nodeManager.leftNode;
+        rightTargetNode = nodeManager.rightNode;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        controller.Move(Vector3.Lerp(transform.position, new Vector3(rightTargetNode.transform.position.x, rightTargetNode.transform.position.x, transform.position.z), Time.deltaTime));	
+	}
+
+
+
+}
---
+++ Assets/PlatformNodeManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformNodeManager : MonoBehaviour {
+
+    public GameObject leftNode;
+    public GameObject rightNode;
+
+    private LineRenderer lineRenderer;
+
+	// Use this for initialization
+	void Start () {
+        lineRenderer = GetComponent<LineRenderer>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        lineRenderer.SetPosition(0, leftNode.transform.position);
+        lineRenderer.SetPosition(1, rightNode.transform.position);
+    }
+}
